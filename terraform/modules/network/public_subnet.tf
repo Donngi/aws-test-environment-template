5b0ac5689e4e99f77c8f4591aa584d0055ab1f05
Donngi <22317839+Donngi@users.noreply.github.com>
2024-03-02 21:54:04 +0900

    Add subnets

---
+++ terraform/modules/network/public_subnet.tf
@@ -0,0 +1,13 @@
+resource "aws_subnet" "public" {
+  count = length(data.aws_availability_zones.available.names)
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  # 2024/3時点で、1リージョンあたりの最大AZ数は6のため、3bit=8分割でIPアドレスを効率的に割り振れる
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 3, count.index * 2)
+
+  tags = {
+    Name = "public-${data.aws_availability_zones.available.names[count.index]}"
+  }
+}
