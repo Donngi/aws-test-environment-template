addd64f6667b915913324129d416d44275fd2a49
Donngi <22317839+Donngi@users.noreply.github.com>
2024-03-03 17:08:18 +0900

    Add vpc endpoint (ec2)

--- terraform/modules/network/vpc_endpoint.tf
+++ terraform/modules/network/vpc_endpoint.tf
@@ -21,3 +21,24 @@ resource "aws_vpc_endpoint" "dynamodb" {
     Environment = "${var.env_name}-dynamodb"
   }
 }
+
+# ------------------------------------------------------------
+# Interface endpoints
+# ------------------------------------------------------------
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.ec2"
+  vpc_endpoint_type = "Interface"
+
+  # 動作検証用の環境のため、冗長性よりもコストを優先し、1つのPrivate subnetにのみ配置
+  subnet_ids = [
+    aws_subnet.private[0].id,
+  ]
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint.id
+  ]
+
+  private_dns_enabled = true
+}
