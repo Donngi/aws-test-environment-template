1d2b206098d68a6703fd30a7a117f102116c7357
Donngi <22317839+Donngi@users.noreply.github.com>
2024-03-03 14:08:24 +0900

    Add route table (private)

---
+++ terraform/modules/network/route_table_private.tf
@@ -0,0 +1,29 @@
+resource "aws_route_table" "private" {
+  count = length(aws_subnet.private)
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = aws_vpc.main.cidr_block
+    gateway_id = "local"
+  }
+
+  dynamic "route" {
+    for_each = var.need_nat_gateway ? ["dummy_value"] : []
+    content {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.main[count.index].id
+    }
+  }
+
+  tags = {
+    Name = aws_subnet.private[count.index].tags["Name"]
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
--- terraform/modules/network/route_table_public.tf
+++ terraform/modules/network/route_table_public.tf
@@ -1,4 +1,6 @@
 resource "aws_route_table" "public" {
+  count = length(aws_subnet.public)
+
   vpc_id = aws_vpc.main.id
 
   route {
@@ -10,11 +12,15 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
+
+  tags = {
+    Name = aws_subnet.public[count.index].tags["Name"]
+  }
 }
 
 resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public)
 
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public[count.index].id
 }
